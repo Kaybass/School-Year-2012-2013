b1be99412bb6eaed55351d159e95f8ba4fe7b9d2
Alexander Bean Apmann <caboose200@gmail.com>
2013-04-08 17:48:50 -0400

    qw

--- comp-eng/Fan/main.cpp
+++ comp-eng/Fan/main.cpp
@@ -10,21 +10,21 @@ int main(){ //is main
 		cin >> y; //is input statement
 		
 		switch(y){ //is switch construct
-		case 'f':
-			cout << "fan go fast now, is bretty gewd\nmany electricity is use\nIf want change speed fan again type one speed letter, followed by enter pressing\n";
-			break;
-		case 'm':
-			cout << "fan go medium speed now, is bretty gewd\nsome electricity is use\nIf want change speed fan again type one speed letter, followed by enter pressing\n";
-			break;
-		case 's':
-			cout << "fan go slow now, is bretty gewd\nlittle electricity is use\nIf want change speed fan again type one speed letter, followed by enter pressing\n";
-			break;
-		case 'o':
-			cout << "fan is turn off now\nhave glorious day comrade\n";
-			break;
-		default:
-			cout << "wrong input was done inputed\nis good idea check alchohol levels\nIf want change speed fan type one speed letter, followed by enter pressing\n";
-			break;
+		    case 'f':
+			    cout << "fan go fast now, is bretty gewd\nmany electricity is use\nIf want change speed fan again type one speed letter, followed by enter pressing\n";
+			    break;
+		    case 'm':
+			    cout << "fan go medium speed now, is bretty gewd\nsome electricity is use\nIf want change speed fan again type one speed letter, followed by enter pressing\n";
+			    break;
+		    case 's':
+			    cout << "fan go slow now, is bretty gewd\nlittle electricity is use\nIf want change speed fan again type one speed letter, followed by enter pressing\n";
+			    break;
+		    case 'o':
+			    cout << "fan is turn off now\nhave glorious day comrade\n";
+			    break;
+		    default:
+			    cout << "wrong input was done inputed\nis good idea check alchohol levels\nIf want change speed fan type one speed letter, followed by enter pressing\n";
+			    break;
 		} //is outside of loop
 	}
 	return 0;
